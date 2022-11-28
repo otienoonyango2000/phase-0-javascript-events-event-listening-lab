6c5205569b12d0d943962fc2dc595b6e450ccdc5
otienoonyango2000 <otienoonyango2000@gmail.com>
2022-11-29 01:29:52 +0300

    event listenner lab completed

--- index.js
+++ index.js
@@ -1,2 +1,14 @@
+
+
 function addingEventListener() {
+    
+    const input = document.getElementById('input');
+
+function clickAlert() {
+  alert('I was clicked!');
 }
+
+input.addEventListener('click', clickAlert);
+
+}
+
